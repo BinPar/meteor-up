f024eabd876ce383d6656bb51ceb3d894238a875
Marcos González Andrés <marcos@binpar.com>
2016-07-26 20:39:23 +0200

    Update start.sh

--- templates/linux/start.sh
+++ templates/linux/start.sh
@@ -6,6 +6,7 @@ BUNDLE_PATH=$APP_PATH/current
 ENV_FILE=$APP_PATH/config/env.list
 PORT=<%= port %>
 USE_LOCAL_MONGO=<%= useLocalMongo? "1" : "0" %>
+DOCKERIMAGE=<%= useMeteor4 ? "mgonand/dockerimages:meteor4" : "mgonand/dockerimages:meteor" %>
 
 # Remove previous version of the app, if exists
 docker rm -f $APPNAME
@@ -15,7 +16,7 @@ docker rm -f $APPNAME-frontend
 
 # We don't need to fail the deployment because of a docker hub downtime
 set +e
-docker pull <% useMeteor4 ? "mgonand/dockerimages:meteor4" : "mgonand/dockerimages:meteor" %>
+docker pull DOCKERIMAGE
 set -e
 
 if [ "$USE_LOCAL_MONGO" == "1" ]; then
@@ -31,7 +32,7 @@ if [ "$USE_LOCAL_MONGO" == "1" ]; then
     --hostname="$HOSTNAME-$APPNAME" \
     --env=MONGO_URL=mongodb://mongodb:27017/$APPNAME \
     --name=$APPNAME \
-    <% useMeteor4 ? "mgonand/dockerimages:meteor4" : "mgonand/dockerimages:meteor" %>
+    DOCKERIMAGE
 else
   docker run \
     -d \
@@ -43,5 +44,5 @@ else
     --env-file=$ENV_FILE \
     --link=mail:mail \
     --name=$APPNAME \
-    <% useMeteor4 ? "mgonand/dockerimages:meteor4" : "mgonand/dockerimages:meteor" %>
+    DOCKERIMAGE
 fi
