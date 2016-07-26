3101ea53175d49471fc343131beeefc043811ea8
Marcos González Andrés <marcos@binpar.com>
2016-07-26 13:05:46 +0200

    Update deploy.sh

--- scripts/linux/deploy.sh
+++ scripts/linux/deploy.sh
@@ -21,36 +21,17 @@ APP_DIR=/opt/<%=appName %>
 
 # save the last known version
 cd $APP_DIR
-if [[ -d current ]]; then
-  sudo rm -rf last
-  sudo mv current last
-fi
 
 # setup the new version
-sudo mkdir current
-sudo cp tmp/bundle.tar.gz current/
-
-# We temporarly stopped the binary building
-# Instead we are building for linux 64 from locally
-# That's just like what meteor do
-# We can have option to turn binary building later on, 
-# but not now
-
-# # rebuild binary module
-# cd current
-# sudo tar xzf bundle.tar.gz
-
-# docker run \
-#   --rm \
-#   --volume=$APP_DIR/current/bundle/programs/server:/bundle \
-#   --entrypoint="/bin/bash" \
-#   meteorhacks/meteord:binbuild -c \
-#     "cd /bundle && bash /opt/meteord/rebuild_npm_modules.sh"
-
-# sudo rm bundle.tar.gz
-# sudo tar czf bundle.tar.gz bundle
-# sudo rm -rf bundle
-# cd ..
+# sudo mkdir current
+if [[ -d current ]]; then
+   cd current
+   rm -rf *.tar.gz
+   cd ..
+   cp tmp/bundle.tar.gz current/
+else
+   mkdir current
+fi
 
 # start app
 sudo bash config/start.sh
