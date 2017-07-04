d323f7bdba992118bd911183cdaae9da5b7bd3e4
Mgonand <marcos@binpar.com>
2017-07-04 13:59:46 +0200

    Fix custom dockerimages and added custom mailName

--- templates/linux/start.sh
+++ templates/linux/start.sh
@@ -7,15 +7,12 @@ ENV_FILE=$APP_PATH/config/env.list
 PORT=<%= port %>
 USE_LOCAL_MONGO=<%= useLocalMongo? "1" : "0" %>
 LINK_MAIL=<%= noMail ? "0" : "1" %>
+MAIL_NAME=<%= mailName %>
 PUBLISH_NETWORK=<%= publishNetwork ? publishNetwork : "127.0.0.1" %>
 DOCKERIMAGE=<%= dockerimage %>
-DOCKERIMAGE=<%= dockerimage ? "mgonand/dockerimages:meteor4" : "mgonand/dockerimages:meteor" %>
 
 # Remove previous version of the app, if exists
 docker rm -f $APPNAME
-
-# Remove frontend container if exists
-docker rm -f $APPNAME-frontend
 
 # We don't need to fail the deployment because of a docker hub downtime
 set +e
@@ -32,7 +29,7 @@ if [ "$USE_LOCAL_MONGO" == "1" ]; then
       --volume=/opt/backups:/backups \
       --env-file=$ENV_FILE \
       --link=mongodb:mongodb \
-      --link=mail:mail \
+      --link=mail:$MAIL_NAME \
       --hostname="$HOSTNAME-$APPNAME" \
       --env=MONGO_URL=mongodb://mongodb:27017/$APPNAME \
       --name=$APPNAME \
@@ -61,7 +58,7 @@ else
       --volume=/opt/backups:/backups \
       --env-file=$ENV_FILE \
       --link=mongodb:mongodb \
-      --link=mail:mail \
+      --link=mail:$MAIL_NAME \
       --hostname="$HOSTNAME-$APPNAME" \
       --name=$APPNAME \
       $DOCKERIMAGE
